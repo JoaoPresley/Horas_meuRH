6005753176ea38b8f9a64b2efd5dc8152f64a6df
João (GEOP) <joao.victor@msgas.com.br>
2026-04-27 10:29:45 -0400

    DEBUG: Validando ausencia de tempo de espera

--- config/config.py
+++ config/config.py
@@ -6,7 +6,8 @@ class Config:
         load_dotenv()
         self.user_name = os.getenv("USER_NAME")
         self.user_password = os.getenv("USER_PASSWORD")
-        self.t_wait = float(os.getenv("T_WAIT", 10)) # Default to 10 seconds if not set
+        val = os.getenv("T_WAIT")
+        self.t_wait = float(val) if val and val.strip() else 10.0 # Default to 10 seconds if not set
         self.rh_online_url = "https://rhonline.msgas.com.br/#/login"
 
 config = Config()
